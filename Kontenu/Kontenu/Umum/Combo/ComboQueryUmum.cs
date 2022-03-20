a4d4b40bfbc874e3824ab777187eed62a423bba6
Santos Sabanari <santos.sabanari@yahoo.com>
2022-03-21 01:32:45 +0700

    update master build

--- Kontenu/Kontenu/Umum/Combo/ComboQueryUmum.cs
+++ Kontenu/Kontenu/Umum/Combo/ComboQueryUmum.cs
@@ -443,5 +443,34 @@ namespace Kontenu.Umum {
 
             return OswCombo.getComboQuery(combo, command, query, parameters, new String[] { "Kode", "Urutan" }, "Kode", "Nama");
         }
+
+        public static LookUpEdit getKategoriMaterial(LookUpEdit combo, MySqlCommand command, Boolean pilihanSemua = false, Boolean pilihanTidakAda = false)
+        {
+            String query = @"(SELECT 100 AS Urutan, kode AS Kode, nama AS Nama 
+                            FROM kategorimaterial)";
+
+            String queryTambahan = "";
+
+            if (pilihanSemua)
+            {
+                queryTambahan += "SELECT 10 AS Urutan, '%' AS Kode, '[Semua]' AS Nama UNION ";
+            }
+
+            if (pilihanTidakAda)
+            {
+                queryTambahan += "SELECT 20 AS Urutan, '' AS Kode, '[Tidak Ada]' AS Nama UNION ";
+            }
+
+            query = queryTambahan + query;
+
+            query = @"SELECT * FROM (" + query + ") Z ORDER BY Urutan, Nama";
+
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+
+            return OswCombo.getComboQuery(combo, command, query, parameters, new String[] { "Kode", "Urutan" }, "Kode", "Nama");
+        }
+
+
+        
     }
 }
